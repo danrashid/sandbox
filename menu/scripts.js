14efa2676e6ce0f99f65e843f8468c8d1f85f785
Dan Rashid <drashid@signal.co>
2015-12-15 15:27:02 -0600

    Adding some click events to menu

--- menu/scripts.js
+++ menu/scripts.js
@@ -1,3 +1,32 @@
+function hideSecondaryNavs() {
+  $('[data-secondary-nav-id], .secondary-nav').removeClass('open');
+}
+
+$('[data-secondary-nav-id]').on('click', function () {
+  var $this = $(this),
+    $secondaryNav = $('#' + $this.data('secondaryNavId'));
+
+  if ($this.hasClass('open')) {
+    hideSecondaryNavs();
+  } else {
+    hideSecondaryNavs();
+    $this.add($secondaryNav).addClass('open');
+  }
+
+  return false;
+});
+
+$('body').on('click', hideSecondaryNavs);
+
+$('[data-expandable-id]').on('click', function () {
+  var $this = $(this),
+    $expandable = $('#' + $this.data('expandableId'));
+
+  $this.add($expandable).toggleClass('open');
+
+  return false;
+});
+
 /*
 
 function setScrollableHeight() {
