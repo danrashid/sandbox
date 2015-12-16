3d58da702fb0b9564d126cfd8104b3ff597d4e9c
Dan Rashid <drashid@signal.co>
2015-12-15 20:33:02 -0600

    Making menu scrolling work

--- menu/scripts.js
+++ menu/scripts.js
@@ -1,5 +1,22 @@
+$.fn.getBBoxHeight = function () {
+  return parseInt($(this).css('height'));
+};
+
 function hideSecondaryNavs() {
   $('[data-secondary-nav-id], .secondary-nav').removeClass('open');
+}
+
+function setScrollableHeight() {
+  $('.secondary-nav.open .scrollable.open').each(function () {
+    var $this = $(this),
+      availableHeight = $this.closest('nav').getBBoxHeight();
+
+    $this.siblings(':not(.scrollable)').each(function () {
+      availableHeight -= $(this).getBBoxHeight();
+    });
+
+    $this.height(Math.min(availableHeight, $this.getBBoxHeight()));
+  });
 }
 
 $('[data-secondary-nav-id]').on('click', function () {
@@ -7,10 +24,26 @@ $('[data-secondary-nav-id]').on('click', function () {
     $secondaryNav = $('#' + $this.data('secondaryNavId'));
 
   if ($this.hasClass('open')) {
-    hideSecondaryNavs();
+    $this.add($secondaryNav).removeClass('open');
   } else {
     hideSecondaryNavs();
     $this.add($secondaryNav).addClass('open');
+    setScrollableHeight();
+  }
+
+  return false;
+});
+
+$('[data-scrollable-id]').on('click', function () {
+  var $this = $(this),
+    $scrollable = $('#' + $this.data('scrollableId'));
+
+  if ($this.hasClass('open')) {
+    $this.add($scrollable).removeClass('open');
+  } else {
+    $('[data-scrollable-id], .scrollable', $this.closest('nav')).removeClass('open');
+    $this.add($scrollable).addClass('open');
+    setScrollableHeight();
   }
 
   return false;
@@ -18,32 +51,4 @@ $('[data-secondary-nav-id]').on('click', function () {
 
 $('body').on('click', hideSecondaryNavs);
 
-$('[data-expandable-id]').on('click', function () {
-  var $this = $(this),
-    $expandable = $('#' + $this.data('expandableId'));
-
-  $this.add($expandable).toggleClass('open');
-
-  return false;
-});
-
-/*
-
-function setScrollableHeight() {
-  $('.scrollable.open').each(function () {
-    var $this = $(this),
-      height = $this.closest('nav').height();
-
-    $this.siblings().each(function () {
-      height -= $(this).height();
-    });
-
-    $this.height(height);
-  });
-}
-
-setScrollableHeight();
-
 $(window).on('resize', setScrollableHeight);
-
-*/
