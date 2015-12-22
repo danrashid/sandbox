17a077dccf84714133aabdcfab4e896a1f66517b
Dan Rashid <drashid@signal.co>
2015-12-22 09:11:12 -0600

    Accounting for top bar in menu

--- menu/scripts.js
+++ menu/scripts.js
@@ -1,55 +1,75 @@
-function hideSecondaryNavs() {
-  $('[data-secondary-nav-id], .secondary-nav').removeClass('open');
-}
+/* global $ */
+'use strict';
 
-function setScrollableHeight() {
-  $('.secondary-nav.open .scrollable.open').each(function () {
-    var $this = $(this),
-      availableHeight = $this.closest('nav').outerHeight(),
-      scrollableHeight = $this.get(0).scrollHeight;
+$(function () {
+  var $navs = $('.primary-nav, .secondary-nav'),
+    topBarHeight = $('.top-bar').outerHeight(),
+    $secondaryNavs = $('[data-secondary-nav-id], .secondary-nav');
 
-    $this.siblings(':not(.scrollable)').each(function () {
-      availableHeight -= $(this).outerHeight();
+  function hideSecondaryNavs() {
+    $secondaryNavs.removeClass('open');
+  }
+
+  function setScrollableHeight() {
+    var $scrollable = $('.secondary-nav.open .scrollable.open'),
+      $secondaryNav = $scrollable.closest('nav'),
+      availableHeight = $secondaryNav.outerHeight() - $secondaryNav.position().top,
+      scrollableHeight = $scrollable.get(0).scrollHeight;
+
+    $scrollable.siblings(':not(.scrollable)').each(function (i, sibling) {
+      availableHeight -= $(sibling).outerHeight();
     });
 
     if (availableHeight < scrollableHeight) {
-      $this.css('height', availableHeight + 'px');
+      $scrollable.css('height', availableHeight + 'px');
     } else if (availableHeight > scrollableHeight) {
-      $this.css('height', '');
+      $scrollable.css('height', '');
     }
+  }
+
+  function setTopPositions() {
+    var windowScrollTop = $(window).scrollTop();
+
+    if (windowScrollTop < topBarHeight) {
+      $navs.css('top', topBarHeight - windowScrollTop);
+    } else {
+      $navs.css('top', 0);
+    }
+  }
+
+  $('[data-secondary-nav-id]').on('click', function (e) {
+    var $secondaryNavTrigger = $(e.target),
+      secondaryNavSelector = '#' + $secondaryNavTrigger.data('secondaryNavId');
+
+    if ($secondaryNavTrigger.hasClass('open')) {
+      $secondaryNavTrigger.add(secondaryNavSelector).removeClass('open');
+    } else {
+      hideSecondaryNavs();
+      $secondaryNavTrigger.add(secondaryNavSelector).addClass('open');
+      setScrollableHeight();
+    }
+
+    return false;
   });
-}
 
-$('[data-secondary-nav-id]').on('click', function () {
-  var $this = $(this),
-    $secondaryNav = $('#' + $this.data('secondaryNavId'));
+  $('[data-scrollable-id]').on('click', function (e) {
+    var $scrollableTrigger = $(e.target),
+      scrollableSelector = '#' + $scrollableTrigger.data('scrollableId');
 
-  if ($this.hasClass('open')) {
-    $this.add($secondaryNav).removeClass('open');
-  } else {
-    hideSecondaryNavs();
-    $this.add($secondaryNav).addClass('open');
-    setScrollableHeight();
-  }
+    if ($scrollableTrigger.hasClass('open')) {
+      $scrollableTrigger.add(scrollableSelector).removeClass('open');
+    } else {
+      $('[data-scrollable-id], .scrollable', $scrollableTrigger.closest('nav')).removeClass('open');
+      $scrollableTrigger.add(scrollableSelector).addClass('open');
+      setScrollableHeight();
+    }
 
-  return false;
+    return false;
+  });
+
+  $('body').on('click', hideSecondaryNavs);
+
+  $(window)
+    .on('scroll resize', setTopPositions)
+    .on('resize', setScrollableHeight);
 });
-
-$('[data-scrollable-id]').on('click', function () {
-  var $this = $(this),
-    $scrollable = $('#' + $this.data('scrollableId'));
-
-  if ($this.hasClass('open')) {
-    $this.add($scrollable).removeClass('open');
-  } else {
-    $('[data-scrollable-id], .scrollable', $this.closest('nav')).removeClass('open');
-    $this.add($scrollable).addClass('open');
-    setScrollableHeight();
-  }
-
-  return false;
-});
-
-$('body').on('click', hideSecondaryNavs);
-
-$(window).on('resize', setScrollableHeight);
